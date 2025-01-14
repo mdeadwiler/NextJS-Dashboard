096cb5449ae64f941bef95cad5ad86b96b5eb2aa
MDeadwiler <mdeadwiler@email.com>
2025-01-13 18:57:03 -0800

    add action.ts action to form test raw form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,1 +1,11 @@
-'user server'
+'use server'
+
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        ammount: formData.get('amount'),
+        stauts: formData.get('status')
+    }
+    // This is a test for raw form data
+    console.log(rawFormData)
+}
