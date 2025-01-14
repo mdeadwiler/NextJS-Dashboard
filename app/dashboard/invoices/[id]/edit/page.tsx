ddae544c88891a35992306cd10a2fb5e7dfca80e
MDeadwiler <mdeadwiler@email.com>
2025-01-14 11:08:05 -0800

    add crud update and delete with tryCatch

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,12 +2,13 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const id = params.id;
   const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
-    fetchCustomers(),
-  ]);
+      fetchInvoiceById(id),
+      fetchCustomers(),  
+    ]);
 
   return (
     <main>
